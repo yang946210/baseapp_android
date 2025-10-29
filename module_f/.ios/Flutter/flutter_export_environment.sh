7aee10c109ba6b05e59eb963494ad5590948b460
yang946210 <284840413@qq.com>
2025-10-29 10:17:56 +0800

    兼容mac本

--- module_f/.ios/Flutter/flutter_export_environment.sh
+++ module_f/.ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\Source\SdkFlutter\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Project\baseapp_android\model_flutter"
+export "FLUTTER_ROOT=/Users/yang/Dev/Flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/yang/Public/WorkSpace/baseapp_android/module_f"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
